4c8cb03b88216a70f110c6a162e472198b02c3cc
MateuszSO48 <u56148@student.wat.edu.pl>
2025-12-08 20:42:11 +0100

    feat: import of home component, circularprogress

--- config/note/frontend/src/App.js
+++ config/note/frontend/src/App.js
@@ -1,10 +1,21 @@
+import {Suspense} from "react";
+import {CircularProgress} from "@mui/material";
 import "./styles/style.scss";
-import Home from "./pages/Home";
+
+import {RouterProvider} from 'react-router-dom';
+import routes from './routes/Router';
 
 function App() {
   return (
-    <div className="App">
-        <Home/>
+    <div className="app">
+        <Suspense
+        fallback={
+            <CircularProgress/>
+        }
+        >
+
+        <RouterProvider router={routes}/>
+        </Suspense>
     </div>
   );
 }
